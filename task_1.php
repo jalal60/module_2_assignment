83b26ae9862af7112b90f5533fd4c569b117bbe1
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-19 23:36:55 +0600

    Task -1 Complete

--- task_1.php
+++ task_1.php
@@ -1,3 +1,40 @@
-
 <?php
-echo "This is Task One";
+//For Loop Function
+function evenNumber1($num1, $num2)
+{
+    echo "For Loop Even Number=";
+    for ($i = $num1; $i <= $num2; $i++) {
+        if ($i % 2 == 0) {
+            echo $i . " ";
+        }
+    }
+}
+evenNumber1(1, 20);
+echo PHP_EOL;
+//While Loop Function
+function evenNumber2($num1, $num2)
+{
+    echo "While Loop Even Number=";
+    $i = $num1;
+    while ($i <= $num2) {
+        if ($i % 2 == 0) {
+            echo $i . " ";
+        }
+        $i++;
+    }
+}
+evenNumber2(1, 20);
+echo PHP_EOL;
+//Do-While Loop Function
+function evenNumber3($num1, $num2)
+{
+    echo "Do While Loop Even Number=";
+    $i = $num1;
+    do {
+        if ($i % 2 == 0) {
+            echo $i . " ";
+        }
+        $i++;
+    } while ($i <= $num2);
+}
+evenNumber3(1, 20);
