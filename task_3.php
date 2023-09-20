f2c875454e5276e664862e30d219f2ad6daab21f
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-20 12:25:50 +0600

    Task 3 Added

---
+++ task_3.php
@@ -0,0 +1,16 @@
+<?php
+	//Fibonacci numbers.
+	$number=0;
+	$num1=0;
+	$num2=1;
+	echo $num1." ".$num2." ";
+	while($number<15){
+	  $num3=$num1+$num2;
+	  if($num3>100)
+	  break;
+	  else if($number<8){
+  	  echo $num3." ";
+  	  $num1=$num2;
+  	  $num2=$num3;
+	    }
+  	  $number++;
